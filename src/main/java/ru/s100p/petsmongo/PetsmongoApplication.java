dfcd8e45681b371eb23ea98a0593a9a265cfad4c
S100P <pro900@gmail.com>
2024-06-22 18:49:43 +0300

    initial commit

---
+++ src/main/java/ru/s100p/petsmongo/PetsmongoApplication.java
@@ -0,0 +1,13 @@
+package ru.s100p.petsmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsmongoApplication.class, args);
+	}
+
+}
